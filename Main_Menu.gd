ffd11a0967f257b51ad11395d3a6a1a743641232
0592326 <0592326@schoolsnet.act.edu.au>
2021-09-03 17:37:56 +1000

    Added new level
    - Cloned testing area for new level.
    - Updated level select menu.
    - Updated scripts to allow the player to open new level.

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -7,6 +7,7 @@ var options_menu
 export (String, FILE) var testing_area_scene
 export (String, FILE) var space_level_scene
 export (String, FILE) var ruins_level_scene
+export (String, FILE) var gloom_level_scene
 
 func _ready():
 	start_menu = $Start_Menu
@@ -22,6 +23,7 @@ func _ready():
 	$Level_Select_Menu/Button_Level_Testing_Area.connect("pressed", self, "level_select_menu_button_pressed", ["testing_scene"])
 	$Level_Select_Menu/Button_Level_Space.connect("pressed", self, "level_select_menu_button_pressed", ["space_level"])
 	$Level_Select_Menu/Button_Level_Ruins.connect("pressed", self, "level_select_menu_button_pressed", ["ruins_level"])
+	$Level_Select_Menu/Button_Level_Gloom.connect("pressed", self, "level_select_menu_button_pressed", ["gloom_level"])
 
 	$Options_Menu/Button_Back.connect("pressed", self, "options_menu_button_pressed", ["back"])
 	$Options_Menu/Button_Fullscreen.connect("pressed", self, "options_menu_button_pressed", ["fullscreen"])
@@ -61,6 +63,9 @@ func level_select_menu_button_pressed(button_name):
 	elif button_name == "ruins_level":
 		set_mouse_and_joypad_sensitivity()
 		get_node("/root/Globals").load_new_scene(ruins_level_scene)
+	elif button_name == "gloom_level":
+		set_mouse_and_joypad_sensitivity()
+		get_node("/root/Globals").load_new_scene(gloom_level_scene)
 
 
 func options_menu_button_pressed(button_name):
